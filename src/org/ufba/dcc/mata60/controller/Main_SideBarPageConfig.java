0429701937984738cb6305e2a387ba9191bdf20e
Caio Lima <ticaiolima@gmail.com>
2014-05-20 00:23:48 -0300

    Adicionando algumas pequenas alterações

--- src/org/ufba/dcc/mata60/controller/Main_SideBarPageConfig.java
+++ src/org/ufba/dcc/mata60/controller/Main_SideBarPageConfig.java
@@ -14,8 +14,8 @@ public class Main_SideBarPageConfig implements SidebarPageConfig{
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
 	
 	public Main_SideBarPageConfig(){		
-		pageMap.put("fn1",new SidebarPage("ufba", "UFBA", "/imagens/icons/add.png", "http://www.ufba.br"));
-		pageMap.put("fn2",new SidebarPage("ufba_dcc", "UFBA DCC", "/imagens/icons/users.png", "http://www.dcc.ufba.br"));
+		pageMap.put("fn1",new SidebarPage("ufba", "UFBA", "/imagens/icons/add.png", "http://localhost:8080/ProjetoMonitoria/teste.zul"));
+		pageMap.put("fn2",new SidebarPage("ufba_dcc", "Listagem", "/imagens/icons/users.png", "http://www.dcc.ufba.br"));
 		pageMap.put("fn3",new SidebarPage("ufba_disciplinas", "UFBA DCC Disciplinas", "/imagens/icons/rss.png", "http://disciplinas.dcc.ufba.br"));
 	}
 	
